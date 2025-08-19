a4758cc917ac715b2d55ab944f817c018e38f1b9
Jumper15 <chordataosteichthyes@gmail.com>
2025-08-19 14:51:29 +1000

    push

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 import _module
 
-# print("hello world")
-
-# print("hello")
-# sockfd = _module.server_config(3001)
-# newsockfd = _module.run_server(sockfd)
-# response = _module.send_response(newsockfd, "MESAGE")
-# print(sockfd, newsockfd, response)
-
-print(_module.res_struct())
+print("hello")
+sockfd = _module.server_config(3001)
+[newsockfd, message] = _module.run_server(sockfd)
+response = _module.send_response(newsockfd, "MESAGE")
+print(sockfd, newsockfd, message, response)
